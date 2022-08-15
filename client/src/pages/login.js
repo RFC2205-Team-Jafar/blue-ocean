53193ef69f031d4c92be4442d856d8617c9b3b07
Andrew <amw.infospecialist@gmail.com>
2022-08-15 13:45:40 -0500

    Firebase Auth file initiated, API verified and commented out

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,8 +1,11 @@
 import React, { useState, useContext } from 'react';
 import { Link } from "react-router-dom";
 import { AllContext } from '../index.jsx';
-import SampleLoginForm from '../components/SampleLoginForm';
+// import SampleLoginForm from '../components/SampleLoginForm.jsx';
+// import {registerWithEmailAndPassword} from '../firebase.jsx'
 
+
+// {registerwithEmailAndPassword("Andrew", "amwbusiness87@gmail.com", "somePassword")}
 const Login = () => {
 
   const { counter } = useContext(AllContext);
@@ -10,12 +13,12 @@ const Login = () => {
 
   return (
     <>
-      <SampleLoginForm />
-
+      {/* <SampleLoginForm /> */}
       <h1> {counter} </h1>
       <div><Link to="/seeker">Redirect to Seeker view </Link></div>
       <div><Link to="/recruiter">Redirect to Recruiter view </Link></div>
       <div><Link to="/">Back to Home</Link></div>
+      {/* <button onClick={registerWithEmailAndPassword("Andrew", "amwbusiness87@gmail.com", "somePassword")}>click here to make an account</button> */}
     </>
   )
 };
