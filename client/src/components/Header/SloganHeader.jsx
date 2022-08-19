64173acb13d34a46e05cfe0137315074ae3381a3
Shirley Nguyen <shirley.nguyen8@outlook.com>
2022-08-19 15:48:29 -0500

    Added Header Styling Options

--- client/src/components/Header/SloganHeader.jsx
+++ client/src/components/Header/SloganHeader.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import { Row } from "../../../public/stylesheets/styles.js";
 import styled from "styled-components";
+// import url("https://fonts.googleapis.com/css?family=Raleway:200");
 
 export default function SloganHeader() {
   return (
@@ -21,7 +22,7 @@ export default function SloganHeader() {
 // `;
 
 const SloganHeaderStyle = styled(Row)`
-  width: 95vw;
+  width: 100vw;
   height: 5em;
   margin: 5px;
   justify-content: center;
@@ -30,4 +31,10 @@ const SloganHeaderStyle = styled(Row)`
     rgba(119, 201, 212, 0.75),
     rgba(87, 188, 144, 0.75)
   );
+  align-items: flex-end;
+  font-size: 14pt;
+  font-weight: light;
+  justify-content: center;
+  color: black;
+  font-family: "Helvetica";
 `;
